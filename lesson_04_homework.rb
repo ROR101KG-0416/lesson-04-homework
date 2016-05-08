13a3199d56e1d37597c523eeb81e5db128c760c1
Alisher778 <internet-docs@mail.ru>
2016-05-08 10:32:17 -0400

    Did some changes for the assignment

--- lesson_04_homework.rb
+++ lesson_04_homework.rb
@@ -1,38 +1,61 @@
 # Code Reading
 
-def fizzbuzz(num)
+def fizzbuzz(num)# it is defined fizzbuzz method with (num)parametr
   # Example comment line
 
-  case
-    when num % 15 == 0 then "FizzBuzz"
-    when num % 3  == 0 then "Fizz"
-    when num % 5  == 0 then "Buzz"
-    else num
-  end
-end
+  case#Case statements consist of an optional condition
+    when num % 15 == 0 then "FizzBuzz" #when the given number modulo 15 and equal 0 prints "FizzBuzz"
+    when num % 3  == 0 then "Fizz"# when the given number modulo is 3 equal to 0 prints "Fizz"
+    when num % 5  == 0 then "Buzz"# when the given number modulo 5 is equal to 0 print "Buzz"
+    else num # another conditions print default (number itself)
+  end # finish case statement
+end #finish the method
 
-def fizz_buzz_to(limit)
-  1.upto(limit).each do |num|
-    puts fizzbuzz(num)
-  end
-end
+def fizz_buzz_to(limit)# we are defining another method fizz_buzz_to with limit parametr
+  1.upto(limit).each do |num|# then we are declaring a block with num parametr
+    puts fizzbuzz(num)# it prints method fizzbuzz with num parametr
+  end# closing method
+end# closing method 
 
 # Fix Broken Code
 
-def area_of_triangle(b, h)
-  puts b x height / 2
+def area_of_triangle(base, height)
+  base * height / 2
 end
 
 base = 7
 height = 6
 
-puts area_of_triangle(height, height)
+puts area_of_triangle(base, height)
 
 # Coding
 
 class Waitlist
-  # add your code here
+  
+  attr_accesor :list, :party
+
+  def initialize(list, party)
+    @list = list
+    @party = party
+  end
+
+  def add_party(party)
+    @list = party
+  end
+
+  def lists(@party)
+    lists.each do |list|.to_i
+      puts "this is #{list}"
+    end
+    
+  end
+
+  def seat
+    @list.shift
+    @list
+  end
 end
+
 
 
 # test case
